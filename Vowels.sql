372837338a6aded45adf376a90f2d845e22cdef4
Kristen Howton <kristen.howton@gmail.com>
2020-10-20 10:30:34 -0500

    ends with and starts with a vowel

---
+++ Vowels.sql
@@ -0,0 +1,5 @@
+﻿-- Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+
+SELECT DISTINCT city
+FROM station
+WHERE city LIKE '[AEIOU]%' AND city LIKE '%[AEIOU]';
