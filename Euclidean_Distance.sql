a7e1f9eddf9c7efdc504f3f66bb470ec3a55e950
Kristen Howton <kristen.howton@gmail.com>
2020-10-20 10:15:46 -0500

    euclidean distance

---
+++ Euclidean_Distance.sql
@@ -0,0 +1,9 @@
+﻿--Consider  and  to be two points on a 2D plane where  are the respective minimum and maximum values of Northern Latitude (LAT_N) and  are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+
+--Query the Euclidean Distance between points  and  and format your answer to display  decimal digits.
+
+SELECT 
+    CAST(SQRT
+        ((POWER(MAX(lat_n) - MIN(lat_n),2)) + 
+        ((POWER(MAX(long_w) - MIN(long_w),2))))AS NUMERIC(10,4))
+FROM Station
