e6391d66dd6d898bd19af2ccacc646379e0f8674
Kristen Howton <kristen.howton@gmail.com>
2020-10-31 14:33:46 -0500

    triangle pattern

---
+++ star-pattern.sql
@@ -0,0 +1,17 @@
+﻿--P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+--* * * * * 
+--* * * * 
+--* * * 
+--* * 
+--*
+--Write a query to print the pattern P(20).
+
+
+DECLARE @var int               -- Declare 
+SELECT @var = 20             -- Initialization 
+WHILE @var > 0                 -- condition 
+BEGIN                          -- Begin 
+PRINT replicate('* ', @var)    -- Print 
+SET @var = @var - 1            -- decrement 
+END                            -- END 
