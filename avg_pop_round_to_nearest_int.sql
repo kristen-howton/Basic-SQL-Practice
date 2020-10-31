85db237ed66573a2c6f05d7b6b0e9450c2cbf859
Kristen Howton <kristen.howton@gmail.com>
2020-10-31 17:42:20 -0500

    round down to nearest int

---
+++ avg_pop_round_to_nearest_int.sql
@@ -0,0 +1,8 @@
+﻿--Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+
+--Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+SELECT co.continent, FLOOR(AVG(c.population))
+FROM city c
+INNER JOIN country co ON co.code = c.countrycode
+GROUP BY co.continent;
