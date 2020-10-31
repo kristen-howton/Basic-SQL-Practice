11f145f93123a2b74777f5b9eb9fd29f31a14bf1
Kristen Howton <kristen.howton@gmail.com>
2020-10-31 18:13:29 -0500

    finding all of japan attributes

---
+++ query_jpn.sql
@@ -0,0 +1,5 @@
+﻿--Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+SELECT id, name, countrycode, district, population
+FROM city
+WHERE countrycode = 'JPN';
