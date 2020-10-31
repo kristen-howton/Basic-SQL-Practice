8f6a7b7186bce3f330c2fbbe9a666cc6ed133c22
Kristen Howton <kristen.howton@gmail.com>
2020-10-31 17:08:57 -0500

    sub query

---
+++ subquery.sql
@@ -0,0 +1,16 @@
+﻿--Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . Round your answer to  decimal places.
+
+--Input Format
+
+--The STATION table is described as follows:
+
+--Station.jpg
+
+--where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+SELECT CAST((long_w) AS decimal(10,4))
+FROM station
+WHERE lat_n =
+    (SELECT MAX(lat_n)
+     FROM station
+     WHERE lat_n < 137.2345);
---
+++ triangle.sql
@@ -0,0 +1,16 @@
+﻿--P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+--* 
+--* * 
+--* * * 
+--* * * * 
+--* * * * *
+--Write a query to print the pattern P(20).
+
+DECLARE @var int               -- Declare 
+SELECT @var = 1             -- Initialization 
+WHILE @var < 21                 -- condition 
+BEGIN                          -- Begin 
+PRINT replicate('* ', @var)    -- Print 
+SET @var = @var + 1            -- increment 
+END                            -- END 
