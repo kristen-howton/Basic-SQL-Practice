ad9a4ccf7752f62cf0041d983ac044ad8dd8b28f
Kristen Howton <kristen.howton@gmail.com>
2020-10-31 17:34:40 -0500

    continet is africa

---
+++ contient_is_africa.sql
@@ -0,0 +1,8 @@
+﻿--Given the CITY and COUNTRY tables, query the names of all cities where the CONTINENT is 'Africa'.
+
+--Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+SELECT c.name
+FROM city c
+INNER JOIN country co ON co.code = c.countrycode
+WHERE continent = 'Africa';
